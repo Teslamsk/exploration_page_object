987cf95bbe7ea35cd4e6db30fb05f2e125d748f5
Alexander Bodoin <aleksandr1borodin@gmail.com>
2020-02-27 00:03:46 +0300

    redesign of the tests

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,7 +1,10 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
@@ -9,12 +12,15 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert True
+        # В методе should_be_login_url реализуйте проверку, что подстрока "login" есть в текущем url браузера.
+        # Для этого используйте соответствующее свойство Webdriver. current_url - Gets the URL of the current page.
+        assert "login" in self.browser.current_url, "Login url is not correct"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,12 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
 
 
 def test_guest_should_see_login_link(browser):
@@ -14,3 +15,26 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
+
+def test_url_mast_contain_login(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+
+def test_guest_should_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+
+def test_guest_should_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+
+# 3. Напишите проверки, используя эти селекторы. Не забудьте через запятую указать адекватное сообщение об ошибке.
+# Напишите сначала красный тест, чтобы убедиться в понятности вывода.
