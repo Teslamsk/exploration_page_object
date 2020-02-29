77c3fcfbbd25d1010dd2b386bddc093caabd9773
Alexander Borodin <aleksandr1borodin@gmail.com>
2020-02-29 15:27:23 +0300

    register some marks in pytest.ini

--- pages/base_page.py
+++ pages/base_page.py
@@ -8,7 +8,7 @@ import math
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-class BasePage(object):
+class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
